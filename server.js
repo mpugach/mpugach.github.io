1dc0f440e20d847b16b55b94bf5b93299b4c9780
Maksym Pugach <pugach.m@gmail.com>
2016-05-06 08:21:35 +0300

    use pug

--- server.js
+++ server.js
@@ -1,6 +1,3 @@
-var connect = require('connect');
-var serveStatic = require('serve-static');
+var pugServer = require('pug-server')
 
-connect().use(serveStatic(__dirname)).listen(8080, function(){
-  console.log('Server running on 8080...');
-});
+pugServer('./src')
