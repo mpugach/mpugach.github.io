1e517b90850af28c0da1ceaac3f88cad882c1ed6
Maksym Pugach <pugach.m@gmail.com>
2016-05-13 07:19:13 +0300

    implement nitifier

--- src/nrs_track_demo/js/notify.js
+++ src/nrs_track_demo/js/notify.js
@@ -1,3 +1,9 @@
 export default function notify(str) {
-  console.log(str);
+  const node = document.createElement('div');
+  const delay = Math.random() * 1000;
+
+  node.appendChild(document.createTextNode(str));
+
+  setTimeout(() => document.getElementById('notifier').appendChild(node), delay);
+  setTimeout(() => node.remove(), delay + 8000);
 }
